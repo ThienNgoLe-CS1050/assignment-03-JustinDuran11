7faa36c507c4fc88beb9f8fa00f7b301e0b645e5
JustinDuran11 <justinduran1105@gmail.com>
2024-06-11 16:36:13 -0600

    done

--- Hello_Github.java
+++ Hello_Github.java
@@ -2,8 +2,18 @@
 public class Hello_Github {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//tasks: 3.1 Declare and assign the following variables:
+		//A String variable named firstName and assign it your first name
+		String firstName = "Justin";
+		//A char variable named middleInitial and assign it your middle initial
+		char middleInitial = 'A';
+		//A String variable named lastName and assign it your last name
+		String lastName = "Duran";
+		//A String variable named myMessage and assign it the message: "Hello Github. Don't host ransomware. Don't perform cyber attacks";
+		String myMessage = "Hello Github. Don't host ransomware. Don't perform cyber attacks.";
+		
+		//3.2 Write a print statement that displays the following format on the console: First M. Last says:"Hello Github Don't host ransomware. Don't perform cyber attacks";
+		System.out.printf("%s %c %s says %s",firstName, middleInitial, lastName, myMessage);			
 	}
 
 }
